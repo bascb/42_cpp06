0be91ecc74efb51d6afc00cc03977b3bce28423b
Bernardo Castelo Branco <bercbranco@gmail.com>
2024-08-22 11:23:36 +0100

    Adding first files

---
+++ ex00/main.cpp
@@ -0,0 +1,135 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bcastelo <bcastelo@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/22 10:40:54 by bcastelo          #+#    #+#             */
+/*   Updated: 2024/08/22 11:09:23 by bcastelo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+#define WIDTH 60
+
+void	print_border( void );
+
+void	print_left(int space);
+
+void	print_rigth(int space);
+
+int	calculate_space(int header_len);
+
+int	calculate_right_space(int header_len);
+
+void	print_header(std::string header);
+
+void	print_comment(std::string comment);
+
+void	print_help(char *prog_name);
+
+int	main(int argc, char **argv)
+{
+	std::string test;
+	
+	for (int i = 1; i < argc; i++)
+	{
+		test = argv[i];
+		if (test == "h" || test == "help")
+		{
+			print_help(argv[0]);
+			return (0);
+		}
+		if (test == "char" || test == "all" || test == "char*")
+		{
+			print_header("Testing Char conversion");
+			
+		}
+	}
+	if (argc == 1 || test == "all")
+	{
+		print_header("Subject example");
+		
+	}
+	return (0);
+}
+
+void	print_border( void )
+{
+	for (int i = 0; i < WIDTH; i++)
+		std::cout << "*";
+	std::cout << std::endl;
+}
+
+void	print_left(int space)
+{
+	std::cout << "*";
+	for (int i = 0; i < space; i++)
+		std::cout << " ";
+}
+
+void	print_rigth(int space)
+{
+	for (int i = 0; i < space; i++)
+		std::cout << " ";
+	std::cout << "*" << std::endl; 
+}
+
+int	calculate_space(int header_len)
+{
+	int	space;
+
+	space = (WIDTH - header_len - 2) / 2;
+	return (space);
+}
+
+int	calculate_right_space(int header_len)
+{
+	int	space;
+
+	space = (WIDTH - header_len - 2) / 2;
+	space += (WIDTH - header_len - 2) % 2;
+	return (space);
+}
+
+void	print_header(std::string header)
+{
+	int space;
+
+	space = calculate_space(header.size());
+	print_border();
+	print_left(space);
+	std::cout << header;
+	space = calculate_right_space(header.size());
+	print_rigth(space);
+	print_border();
+}
+
+void	print_comment(std::string comment)
+{
+	int space;
+
+	space = calculate_space(comment.size());
+	std::cout << std::endl;
+	print_left(space);
+	std::cout << comment;
+	space = calculate_right_space(comment.size());
+	print_rigth(space);
+	std::cout << std::endl;
+}
+
+void	print_help(char *prog_name)
+{
+	print_header("Options and usage");
+	std::cout << "Options:" << std::endl;
+	std::cout << std::endl;
+	std::cout << "char - Testing Char conversion" << std::endl;
+	std::cout << "all - Run all tests" << std::endl;
+	std::cout << std::endl;
+	std::cout << "Usage example:" << std::endl;
+	std::cout << std::endl;
+	std::cout << prog_name << " shrub_const" << std::endl;
+	std::cout << std::endl;
+}
